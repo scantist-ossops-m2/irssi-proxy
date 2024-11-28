31ac50724a4f679762827c2f4a70c96fba7b9ab5
turly221 <feifan@scntist.com>
2024-11-28 03:55:17 +0000

    commit patch 7394153

--- src/core/misc.c
+++ src/core/misc.c
@@ -756,8 +756,11 @@ int expand_escape(const char **data)
 		*data += 2;
 		return strtol(digit, NULL, 16);
 	case 'c':
-                /* control character (\cA = ^A) */
-                (*data)++;
+		/* check for end of string */
+		if ((*data)[1] == '\0')
+			return 0;
+		/* control character (\cA = ^A) */
+		(*data)++;
 		return i_toupper(**data) - 64;
 	default:
 		if (!i_isdigit(**data))
